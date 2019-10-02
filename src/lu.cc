0a055916c0793788963368590c945f7cb2cce3b9
David Kleingeld <dskleingeld@gmail.com>
2019-10-02 12:56:54 +0200

    adds skeletoncode and assignment

---
+++ src/lu.cc
@@ -0,0 +1,98 @@
+#include <cstdio>
+#include <ctime>
+
+#include "matrix.h"
+
+/* Code taken from the GLIBC manual.
+ *
+ * Subtract the ‘struct timespec’ values X and Y,
+ * storing the result in RESULT.
+ * Return 1 if the difference is negative, otherwise 0.
+ */
+static int
+timespec_subtract (struct timespec *result,
+                   struct timespec *x,
+                   struct timespec *y)
+{
+  /* Perform the carry for the later subtraction by updating y. */
+  if (x->tv_nsec < y->tv_nsec) {
+    int nsec = (y->tv_nsec - x->tv_nsec) / 1000000000 + 1;
+    y->tv_nsec -= 1000000000 * nsec;
+    y->tv_sec += nsec;
+  }
+  if (x->tv_nsec - y->tv_nsec > 1000000000) {
+    int nsec = (x->tv_nsec - y->tv_nsec) / 1000000000;
+    y->tv_nsec += 1000000000 * nsec;
+    y->tv_sec -= nsec;
+  }
+
+  /* Compute the time remaining to wait.
+     tv_nsec is certainly positive. */
+  result->tv_sec = x->tv_sec - y->tv_sec;
+  result->tv_nsec = x->tv_nsec - y->tv_nsec;
+
+  /* Return 1 if result is negative. */
+  return x->tv_sec < y->tv_sec;
+}
+
+
+/* Global variables holding the matrix data. To complete this assignment
+ * you are requested to only use arrays and access these arrays with
+ * subscripts. Do not use pointers.
+ */
+
+const int max_n_elements = 131072;
+const int max_n_rows = 16384;
+
+static double values[max_n_elements];
+
+static int col_ind[max_n_elements];
+static int row_ptr_begin[max_n_rows];
+static int row_ptr_end[max_n_rows];
+
+
+
+
+int
+main(int argc, char **argv)
+{
+  if (argc != 2)
+    {
+      fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+      return -1;
+    }
+
+  int nnz, n_rows, n_cols;
+  bool ok(false);
+
+  ok = load_matrix_market(argv[1], max_n_elements, max_n_rows,
+                          nnz, n_rows, n_cols,
+                          values, col_ind, row_ptr_begin, row_ptr_end);
+  if (!ok)
+    {
+      fprintf(stderr, "failed to load matrix.\n");
+      return -1;
+    }
+
+  /* For debugging, can be removed when implementation is finished. */
+  dump_nonzeros(n_rows, values, col_ind, row_ptr_begin, row_ptr_end);
+
+
+  struct timespec start_time;
+  clock_gettime(CLOCK_REALTIME, &start_time);
+
+  /* Perform LU factorization here */
+
+  struct timespec end_time;
+  clock_gettime(CLOCK_REALTIME, &end_time);
+
+
+  struct timespec elapsed_time;
+  timespec_subtract(&elapsed_time, &end_time, &start_time);
+
+  double elapsed = (double)elapsed_time.tv_sec +
+      (double)elapsed_time.tv_nsec / 1000000000.0;
+  fprintf(stderr, "elapsed time: %f s\n", elapsed);
+
+  return 0;
+}
